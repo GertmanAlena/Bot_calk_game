4b6a299fbbf7007ba3595e5e015e05be602b77df
Alena <gertmanea@mail.ru>
2022-10-05 22:14:30 +0300

    .\bot_calk\

--- bot_calk/bot_command.py
+++ bot_calk/bot_command.py
@@ -1,1 +1,26 @@
-﻿
+﻿from distutils.command.config import config
+from markupsafe import Markup
+from telegram import *
+from telegram.ext import Updater, CommandHandler, Filters, MessageHandler, ConversationHandler, CallbackContext
+from anecAPI import anecAPI
+from calk import *
+import telebot
+
+
+def start(update, context):
+    arg = context.args
+    # arg = del_element(text)
+    if not arg:
+        context.bot.send_message(update.effective_chat.id, "Привет")
+    else:
+        context.bot.send_message(update.effective_chat.id, f"{' '.join(arg)}")
+
+def exit_add(update, context):
+    context.bot.send_message(
+        update.effective_chat.id, f'Операция прервана')
+    return ConversationHandler.END
+
+
+
+        
+
